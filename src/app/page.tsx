8f0480464296d581e025c59274ca51c80f90ed52
Lonka Pardhu <lonkapardhu99@gmail.com>
2024-10-15 07:58:46 +0530

    oAuth setup feat server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@ import Image from "next/image";
 import Header from "@/components/ui/header";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div>
       <Header />
-      <h1>Welcome Back to your prime</h1>
+      <div className="h-screen w-full flex items-center justify-center">
+        <h1>Work in progress...</h1>
+      </div>
     </div>
   );
 }
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -23,14 +23,25 @@ const Header = async (props: Props) => {
   console.log(session);
   return (
     <header>
-      <nav>
+      <nav className="w-full p-2 flex flex-row items-center justify-between mx-auto border-b border-1">
         <h1>Trustimonial</h1>
-        <div className="w-full p-4 flex flex-row justify-between items-center">
+        <div>
           {session?.user ? (
-            <>
-              <h2>{session.user.name}</h2>
+            <div className="flex flex-row items-center gap-x-2">
+              <div className="flex flex-col items-center gap-y-1">
+                {session?.user?.image && session?.user?.name && (
+                  <Image
+                    src={session.user.image}
+                    alt={session.user.name}
+                    width={35}
+                    height={35}
+                    className="rounded-full"
+                  />
+                )}
+                <p className="text-sm">{session.user.name}</p>
+              </div>
               <SignOut />
-            </>
+            </div>
           ) : (
             <Link href="/auth/signin">
               <Button>Sign In</Button>
